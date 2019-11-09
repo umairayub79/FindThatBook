918a37bbfa56a75f0ed8680a22eef6c2689716ce
umairayub79 <umairayub79@gmail.com>
2019-11-09 20:59:21 +0500

    Implemented SearchResult Activity

--- app/src/main/java/codes/umair/findthatbook/activities/MainActivity.java
+++ app/src/main/java/codes/umair/findthatbook/activities/MainActivity.java
@@ -1,33 +1,21 @@
 package codes.umair.findthatbook.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.app.Application;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import codes.umair.findthatbook.App;
+import androidx.appcompat.app.AppCompatActivity;
+
 import codes.umair.findthatbook.R;
-import codes.umair.findthatbook.adapters.BookListAdapter;
-import codes.umair.findthatbook.api.BooksAPI;
-import codes.umair.findthatbook.models.SearchResult;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn_submit;
     EditText edt_query;
 
-    BookListAdapter adapter;
-    RecyclerView recyclerView;
-    BooksAPI api;
-    public static final int MAX_RESULTS = 40;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,42 +23,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btn_submit = findViewById(R.id.btn_submit);
         edt_query = findViewById(R.id.edit_query);
-        recyclerView = findViewById(R.id.recyclerV);
 
-        adapter = new BookListAdapter();
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
 
-        App app = (App) getApplication();
-        api = app.getApi();
         btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (!edt_query.getText().toString().isEmpty()) {
                     String query = edt_query.getText().toString();
-                    SearchBook(query);
+                    Intent i = new Intent(MainActivity.this, SearchResultsActivity.class);
+                    i.putExtra("query", query);
+                    startActivity(i);
                 }
 
             }
         });
     }
 
-    public void SearchBook(String query) {
-        Call<SearchResult> call = api.searchBooks(query, MAX_RESULTS);
-        call.enqueue(new Callback<SearchResult>() {
-            @Override
-            public void onResponse(Call<SearchResult> call, Response<SearchResult> response) {
-                SearchResult result = response.body();
-                if (result != null && result.getTotalItems() > 0) {
-                    adapter.setData(MainActivity.this, result.getBooks());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<SearchResult> call, Throwable t) {
-
-            }
-        });
-
-    }
 }
---
+++ app/src/main/java/codes/umair/findthatbook/activities/SearchResultsActivity.java
@@ -0,0 +1,68 @@
+package codes.umair.findthatbook.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import codes.umair.findthatbook.App;
+import codes.umair.findthatbook.R;
+import codes.umair.findthatbook.adapters.BookListAdapter;
+import codes.umair.findthatbook.api.BooksAPI;
+import codes.umair.findthatbook.models.SearchResult;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class SearchResultsActivity extends AppCompatActivity {
+
+    public static final int MAX_RESULTS = 40;
+    BookListAdapter adapter;
+    RecyclerView recyclerView;
+    ProgressBar progressBar;
+    BooksAPI api;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+        recyclerView = findViewById(R.id.recyclerV);
+        progressBar = findViewById(R.id.progress_circular);
+
+        adapter = new BookListAdapter();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        App app = (App) getApplication();
+        api = app.getApi();
+
+        Intent i = getIntent();
+        String query = i.getStringExtra("query");
+        SearchBook(query);
+    }
+
+    public void SearchBook(String query) {
+        Call<SearchResult> call = api.searchBooks(query, MAX_RESULTS);
+        call.enqueue(new Callback<SearchResult>() {
+            @Override
+            public void onResponse(Call<SearchResult> call, Response<SearchResult> response) {
+                SearchResult result = response.body();
+                if (result != null && result.getTotalItems() > 0) {
+                    adapter.setData(SearchResultsActivity.this, result.getBooks());
+                    progressBar.setVisibility(View.GONE);
+                    recyclerView.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<SearchResult> call, Throwable t) {
+
+            }
+        });
+
+    }
+}
